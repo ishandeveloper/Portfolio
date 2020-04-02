667e29b8c83c84b2eb3884a77fdd321b30651617
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-02 16:10:41 +0530

    Service worker

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -1,10 +1,10 @@
 const staticCacheName = 'site-static-v1';
 const assets = [
-    '/',
-    '/index.html',
+    '/Portfolio/',
+    '/Portfolio/index.html',
     '/style.css',
-    '/assets/img/mobile.png',
-    '/assets/img/favicon.png',
+    '/Portfolio/assets/img/mobile.png',
+    '/Portfolio/assets/img/favicon.png',
     'https://fonts.googleapis.com/css?family=Lato:300,400,700'
 ];
 // install event
