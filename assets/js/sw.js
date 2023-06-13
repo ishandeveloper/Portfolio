bafc8d88f3cbd300a1163f2e8d33afec198c27f4
cagkap <caglakap01@gmail.com>
2023-06-13 19:56:56 +0300

    last changes

--- assets/js/sw.js
+++ assets/js/sw.js
@@ -7,7 +7,7 @@ const assets = [
     '/Portfolio/404.html',
     '/style.css',
     '/Portfolio/assets/img/mobile.png',
-    '/Portfolio/assets/img/favicon.png',
+    
     'https://fonts.googleapis.com/css?family=Lato:300,400,700'
 ];
 // install event
